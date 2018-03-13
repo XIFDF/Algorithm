80f20b12c2b27db3100267bb8b9b59246a1f8c70
Yijue Peng <2398869479@qq.com>
2018-03-13 19:12:42 +0800

    Add files via upload

---
+++ sort/Selection sort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void selection_sort(vector<int> &array) {
+	for (int i = 0; i < array.size() - 1; ++i) {
+		int minIndex = i;
+		for (int j = i + 1; j < array.size(); ++j) {
+			if (array[j] < array[minIndex]) {
+				minIndex = j;
+			}
+		}
+		swap(array[i], array[minIndex]);
+	}
+}
+
+
+int main() {
+	vector<int> array = { 64, 45, 34, 90, 12, 5, 33, 65 };
+	selection_sort(array);
+	for (auto a : array) {
+		cout << a << " ";
+	}
+	cout << endl;
+	int i;
+	cin >> i;
+	return 0;
+}
