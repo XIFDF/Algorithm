84b295c0e4a90399793a7887b206333c80f90ebf
Yijue Peng <2398869479@qq.com>
2018-08-20 17:21:01 +0800

    Update Add two nums .java

--- LeetCode/Add two nums .java
+++ LeetCode/Add two nums .java
@@ -1,3 +1,13 @@
+/*
+给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+
+示例：
+输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+输出：7 -> 0 -> 8
+原因：342 + 465 = 807
+*/
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -7,6 +17,8 @@
  * }
  */
 class Solution {
+    //就是从低位到高位的加法运算细节，要注意最高位的运算后是否再增一位
+    //注意引用的情况(是否为空)
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode p1 = l1, p2 = l2, cur = head;
